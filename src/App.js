7ea88ae5774ff787bd6b3e001acfe4660c591afe
neelabja321 <neelabja321@gmail.com>
2023-05-19 10:32:54 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import Navbar from "./components/Navbar.js";
+import TextForm from "./components/TextForm.js";
+import Alert from "./components/Alert";
+// import About from "./components/About.js";
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
+  const [mode, setMode] = useState("light"); // whether dark mode is enabled or not
+  // const [redMode, setRedMode] = useState("light");
+  const [alert, setAlert] = useState(null);
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type,
+    });
+    setTimeout(() => {
+      setAlert(null);
+    }, 2000);
+  };
+  const toggleMode = () => {
+    if (mode === "light") {
+      setMode("dark");
+      document.body.style.backgroundColor = "#090d20";
+      showAlert("Dark mode has been enabled", "success");
+    } else {
+      setMode("light");
+      document.body.style.backgroundColor = "white";
+      showAlert("Light mode has been enabled", "success");
+    }
+  };
+  // const redToggleMode = () => {
+  //   if (redMode === "light") {
+  //     setRedMode("green");
+  //     document.body.style.backgroundColor = "#831a1a";
+  //     showAlert("Red theme has been enabled", "success");
+  //   } else {
+  //     setRedMode("light");
+  //     document.body.style.backgroundColor = "white";
+  //     showAlert("Light mode has been enabled", "success");
+  //   }
+  // };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {/* <Navbar
+        title="TEXT UTILS"
+        aboutText="About Us"
+        mode={mode}
+        toggleMode={toggleMode}
+        // redToggleMode={redToggleMode}
+      /> */}
+      {/* <Router basename="/"> */}
+      <Navbar
+        title="TEXT UTILS"
+        aboutText="About Us"
+        mode={mode}
+        toggleMode={toggleMode}
+        // redToggleMode={redToggleMode}
+      />
+      <Alert alert={alert} />
+      <div className="container my-3">
+        {/* <Routes> */}
+        {/* <Route exact path="/about" element={<About />}></Route> */}
+        {/* <Route */}
+        {/* exact path="/" */}
+        {/* element={ */}
+        <TextForm
+          showAlert={showAlert}
+          heading="Enter the text to analyze"
+          mode={mode}
+        />
+        {/* } */}
+        {/* ></Route> */}
+        {/* </Routes> */}
+      </div>
+      {/* </Router> */}
+    </>
   );
 }
 
---
+++ src/components/TextForm.js
@@ -0,0 +1,103 @@
+import React, { useState } from "react";
+
+export default function TextForm(props) {
+  const handleUpClick = () => {
+    let newText = text.toUpperCase();
+    setText(newText);
+    newText !== ""
+      ? props.showAlert("converted to upper case", "success")
+      : props.showAlert("Please enter text to continue", "warning");
+  };
+
+  const handleOnChange = (event) => {
+    setText(event.target.value);
+  };
+
+  const handleDownClick = () => {
+    let newText = text.toLowerCase();
+    setText(newText);
+    newText !== ""
+      ? props.showAlert("converted to lower case", "success")
+      : props.showAlert("Please enter text to continue", "warning");
+  };
+
+  const [text, setText] = useState("");
+
+  const handleCleartext = () => {
+    let newText = "";
+    setText(newText);
+    text !== ""
+      ? props.showAlert("text has been cleared", "success")
+      : props.showAlert("Please enter text to continue", "warning");
+  };
+
+  return (
+    <>
+      <div
+        className="container"
+        style={{ color: props.mode === "dark" ? "white" : "black" }}
+      >
+        <div className="mb-3">
+          <h1>{props.heading}</h1>
+          <textarea
+            className={"form-control"}
+            value={text}
+            style={{
+              backgroundColor: props.mode === "dark" ? "grey" : "white",
+              color: props.mode === "dark" ? "white" : "black",
+            }}
+            onChange={handleOnChange}
+            id="myBox"
+            rows="8"
+          ></textarea>
+        </div>
+        <button className="btn btn-primary mx-1 my-2" onClick={handleUpClick}>
+          Convert to upperCase
+        </button>
+        <button className="btn btn-primary mx-1" onClick={handleDownClick}>
+          Convert to lowerCase
+        </button>
+        <button className="btn btn-primary mx-1" onClick={handleCleartext}>
+          Clear Text
+        </button>
+      </div>
+      <div
+        className={`container my-4`}
+        style={{ color: props.mode === "dark" ? "white" : "black" }}
+      >
+        <h2 className={props.mode === "dark" ? "text-light" : "text-dark"}>
+          Your text summary
+        </h2>
+        <p>
+          <b>
+            {text.length === 0
+              ? text.length
+              : text.split(" ")[text.split(" ").length - 1] === ""
+              ? text.split(" ").length - 1
+              : text.split(" ").length}
+          </b>{" "}
+          words and <b>{text.length}</b> charecters
+        </p>
+        <p>
+          <b>
+            {text.length === 0
+              ? "0.00"
+              : 0.008 * text.split(" ").length.toFixed(2)}
+          </b>{" "}
+          minuites to read
+        </p>
+        <div
+          className="container"
+          style={{ color: props.mode === "dark" ? "white" : "black" }}
+        >
+          <h2>Preview</h2>
+          <p>
+            {text.length > 0
+              ? text
+              : "Enter something in textbox text to preview"}
+          </p>
+        </div>
+      </div>
+    </>
+  );
+}
